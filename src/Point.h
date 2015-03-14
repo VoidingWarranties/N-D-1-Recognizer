c229ccc92731882b09ba11549ccf562a35cece61
Jordan Horwich <jordan@voidingwarranties.com>
2015-03-14 16:09:12 -0700

    Overload operator<< for Points

--- src/Point.h
+++ src/Point.h
@@ -11,6 +11,15 @@ class Point {
   const float& operator[](std::size_t i) const { return dimensions_[i]; }
   float& operator[](std::size_t i) { return dimensions_[i]; }
 
+  friend std::ostream& operator<<(std::ostream& out, const Point<N>& p) {
+    out << "(";
+    for (std::size_t i = 0; i < N - 1; ++i) {
+      out << p.dimensions_[i] << ",";
+    }
+    out << p.dimensions_[N-1] << ")";
+    return out;
+  }
+
  private:
   float dimensions_[N];
 };
